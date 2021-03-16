adae715ffa26716a49cb28b09dc33295ef372d85
tobil <tobilesi@gmail.com>
2021-03-16 18:57:21 +0000

    scroll feature added with issues

--- src/components/PageScroll.js
+++ src/components/PageScroll.js
@@ -4,13 +4,20 @@ import IntroSpinner from './IntroSpinner'
 import Mouse from './Mouse'
 import 'react-typist/dist/Typist.css'
 
+// import tb from '../images/tb2.png'
+
 import Typical from 'react-typical'
 
 function PageScroll() {
 
   const [scrollValue, setScrollValue] = React.useState(0)
+  const [slideValue, setSlideValue] = React.useState(0)
   const [loading, setLoading] = React.useState(false)
   const [typing, setTyping] = React.useState(true)
+  const [logoSlide, setLogoSlide] = React.useState(false)
+  const [blackSlide, setBlackSlide] = React.useState(false)
+  const [logoValue, setLogoValue] = React.useState(0)
+
 
 
 
@@ -20,21 +27,28 @@ function PageScroll() {
     setTyping(true)
     setTimeout(() => {
       setLoading(false)
-    }, 8000) 
+    }, 1000)
     const handleIsScrolling = () => {
-      setScrollValue(window.scrollY)
-      if (window.scrollY > 0){
-        setTyping(false)
-      } else {
-        setTyping(true)
-      }
+
+      window.scrollY <= 100 ? setScrollValue(window.scrollY) : setScrollValue(100)
+
+      setSlideValue(window.scrollY)
+
+      window.scrollY >= 180 ? setLogoSlide(true) : setLogoSlide(false)
+
+      window.scrollY > 0 ? setTyping(false) : setTyping(true)
+
+
+      setLogoValue(window.scrollY)
+
+      window.scrollY >= 690 ? setBlackSlide(true) : setBlackSlide(false)
+
 
     }
     window.addEventListener('scroll', handleIsScrolling)
 
   }, [])
-
-
+  console.log(window.scrollY)
 
   return (
     <>
@@ -51,7 +65,7 @@ function PageScroll() {
                       <div>
                         <Typical
                           steps={['', 500, 'Hi there,', 1000, 'Welcome to my Construction Page.', 1000, 'Portfolio is on the way this is just a litte promo.', 1000, 'Scroll Down.']}
-                          wrapper="p" 
+                          wrapper="p"
                         />
                       </div>
                       :
@@ -60,16 +74,43 @@ function PageScroll() {
                         <Typical
                           loop={0}
                           steps={['Keep Scrolling']}
-                          wrapper="p" 
+                          wrapper="p"
                         />
                       </div>
                   }
                 </div>
               </div>
-              <h3 className="text" style={{ 'left': `${100 - scrollValue / 5}%` }}>Hey There Welcome to My Portfolio</h3>
-              <section className="first-page-text" style={{ 'clipPath': `circle(${scrollValue}px at center center)` }}>
-                <h3 className="inner-text" style={{ 'left': `${100 - scrollValue / 5}%` }}>Hey There Welcome to My Portfolio</h3>
-              </section>
+              <h3 className="text" style={{ 'left': `${100 - slideValue / 5}%` }}>Hey There Welcome to My Portfolio</h3>
+              <>
+                {
+                  logoSlide ?
+                    <section className="first-page-text" style={{
+                      'clipPath': `circle(${scrollValue}px at center center)`, 'left': `${36 - logoValue / 5}%`,
+                    }}>
+                      <h3 className="inner-text" style={{ 'left': `${100 - slideValue / 5}%` }}>Hey There Welcome to My Portfolio</h3>
+                    </section>
+
+                    :
+
+                    <section className="first-page-text" style={{
+                      'clipPath': `circle(${scrollValue}px at center center)`,
+                    }}>
+                      <h3 className="inner-text" style={{ 'left': `${100 - slideValue / 5}%` }}>Hey There Welcome to My Portfolio</h3>
+                    </section>
+                }
+              </>
+              {
+                blackSlide ?
+                  <div className="black-container">
+                    <div className="black-slide" style={{ 'left': `${280 - slideValue / 4}%` }}>
+
+                    </div>
+                  </div>
+                  :
+                  <div className="black-container">
+                    <div className="black-slide"></div>
+                  </div>
+              }
               <div className="container">
                 <Mouse />
               </div>
