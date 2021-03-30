c0256d0ddf6e14e08c10d997d2db483f43160227
tobil <tobilesi@gmail.com>
2021-03-30 22:04:53 +0100

    second section layed out ish

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,108 +5,98 @@ import arch from '../images/iconsGifs/intro-arch.gif'
 import udemy from '../images/iconsGifs/Udemy.png'
 import fiver from '../images/iconsGifs/Fiverr.png'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-
-
-
-function useOnScreen(options) {
-
-  const ref = React.useRef()
-  const [visible, setVisible] = React.useState(false)
-
-  React.useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      setVisible(entry.isIntersecting)
-    }, options)
-
-    if (ref.current) {
-      observer.observe(ref.current)
-    }
-
-    return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current)
-      }
-    }
-
-  }, [ref, options])
-
-  return [ref, visible]
-}
-
-
-
-
 function AboutMe() {
-
-  const [ref, visible] = useOnScreen({ rootMargin: '700px' })
 
   return (
 
     <>
       <div className="black-container">
-        <Swiper
-          direction={'horizontal'}
-          effect={'cube'}
-          grabCursor={true }
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          autoplay={true}
-        >
-          <SwiperSlide>
-            <div className="category-container">
-              <div className="category-gif arch" style={{ backgroundImage: `url(${arch})` }}></div>
+        <div className="box-area">
+          <div className="category-container">
+            <div className="category">
+              <div className="category-wrapper">
+                <div className="info">
+                  <div
+                    className="info-logo">
+                    <img src={arch} />
+                  </div>
+                </div>
+              </div>
               <div className="info-wrapper">
                 <div className="info">
                   <div
-                    ref={ref}
-                    className={`info-logo ${visible ? 'fade-inLogo' : 'fade-outLogo'}`}>
+                    className="info-logo">
                     <img src={uol} />
                   </div>
                   <div
-                    ref={ref}
-                    className={`info-text ${visible ? 'fade-inLogo' : 'fade-outLogo'}`}>
+                    className="info-text">
                     <p>Student at XJTLU/ graduate of the University of Liverpool specialising in architecture practice.</p>
                     <p>I am skilled in special programs and design development is a strong interest for me.</p>
                   </div>
                 </div>
               </div>
             </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="category-container">
-              <div className="category-gif arch" style={{ backgroundImage: `url(${arch})` }}></div>
+          </div>
+          <div className="category-container">
+            <div className="category">
+              <div className="category-wrapper">
+                <div className="info">
+                  <div
+                    className="info-logo">
+                    <img src={arch} />
+                  </div>
+                </div>
+              </div>
               <div className="info-wrapper">
                 <div className="info">
                   <div
-                    ref={ref}
-                    className="info-logo-gd">
-                    <img src={udemy} />
-                    <img src={fiver} />
+                    className="info-logo">
+                    <img src={uol} />
                   </div>
                   <div
-                    ref={ref}
                     className="info-text">
-                    <p>I am currently studying China Studies as part of my year abroad.  </p>
+                    <p>Student at XJTLU/ graduate of the University of Liverpool specialising in architecture practice.</p>
+                    <p>I am skilled in special programs and design development is a strong interest for me.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="category-container">
+            <div className="category">
+              <div className="category-wrapper">
+                <div className="info">
+                  <div
+                    className="info-logo">
+                    <img src={arch} />
+                  </div>
+                </div>
+              </div>
+              <div className="info-wrapper gd">
+                <div className="gd-item">
+                  <div className="gd-logo" >
+                    <img src={udemy} />
+                  </div>
+                  <div className="gd-content">
+                    <p>I am currently studying China Studies as part of my year abroad.</p>
+                  </div>
+                </div>
+                <div className="gd-item">
+                  <div className="gd-logo">
+                    <img src={fiver} />
+                  </div>
+                  <div className="gd-content">
                     <p>My goal is to practice my knowledge and learn a new skill set at an architectural practice in London.</p>
                   </div>
                 </div>
               </div>
             </div>
-          </SwiperSlide>
-          
-        </Swiper>
+          </div>
 
-      </div>
-      <div className="gd-container">
-        
-
+        </div>
       </div>
     </>
   )
-
-
 }
+
 export default AboutMe
