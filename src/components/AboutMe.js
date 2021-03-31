be31d5444f432eae6a145efde8f8ef8fcd8f3529
tobil <tobilesi@gmail.com>
2021-03-31 15:40:00 +0100

    second section cube rotation working

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,88 +11,70 @@ function AboutMe() {
 
     <>
       <div className="black-container">
-        <div className="box-area">
-          <div className="category-container">
-            <div className="category">
-              <div className="category-wrapper">
-                <div className="info">
-                  <div
-                    className="info-logo">
-                    <img src={arch} />
+        <div className="section2-wrapper">
+          <div className="box-area">
+            <div className="category-container arch">
+              <div className="category">
+                <div className="category-wrapper">
+                  <div className="info">
+                    <div
+                      className="info-logo">
+                      <img src={arch} />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="info-wrapper">
-                <div className="info">
-                  <div
-                    className="info-logo">
-                    <img src={uol} />
-                  </div>
-                  <div
-                    className="info-text">
-                    <p>Student at XJTLU/ graduate of the University of Liverpool specialising in architecture practice.</p>
-                    <p>I am skilled in special programs and design development is a strong interest for me.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="category-container">
-            <div className="category">
-              <div className="category-wrapper">
-                <div className="info">
-                  <div
-                    className="info-logo">
-                    <img src={arch} />
-                  </div>
-                </div>
-              </div>
-              <div className="info-wrapper">
-                <div className="info">
-                  <div
-                    className="info-logo">
-                    <img src={uol} />
-                  </div>
-                  <div
-                    className="info-text">
-                    <p>Student at XJTLU/ graduate of the University of Liverpool specialising in architecture practice.</p>
-                    <p>I am skilled in special programs and design development is a strong interest for me.</p>
+                <div className="info-wrapper">
+                  <div className="info">
+                    <div
+                      className="info-logo">
+                      <img src={uol} />
+                    </div>
+                    <div
+                      className="info-text">
+                      <p>Student at XJTLU/ graduate of the University of Liverpool specialising in architecture practice.</p>
+                      <p>I am skilled in special programs and design development is a strong interest for me.</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="category-container">
-            <div className="category">
-              <div className="category-wrapper">
-                <div className="info">
-                  <div
-                    className="info-logo">
-                    <img src={arch} />
+            <div className="category-container gd">
+              <div className="category">
+                <div className="category-wrapper">
+                  <div className="info">
+                    <div
+                      className="info-logo">
+                      <img src={arch} />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="info-wrapper gd">
-                <div className="gd-item">
-                  <div className="gd-logo" >
-                    <img src={udemy} />
+                <div className="info-wrapper">
+                  <div className="gd-item">
+                    <div className="gd-logo" >
+                      <img src={udemy} />
+                    </div>
+                    <div className="gd-content">
+                      <p>I am currently studying China Studies as part of my year abroad.</p>
+                    </div>
                   </div>
-                  <div className="gd-content">
-                    <p>I am currently studying China Studies as part of my year abroad.</p>
-                  </div>
-                </div>
-                <div className="gd-item">
-                  <div className="gd-logo">
-                    <img src={fiver} />
-                  </div>
-                  <div className="gd-content">
-                    <p>My goal is to practice my knowledge and learn a new skill set at an architectural practice in London.</p>
+                  <div className="gd-item">
+                    <div className="gd-logo">
+                      <img src={fiver} />
+                    </div>
+                    <div className="gd-content">
+                      <p>My goal is to practice my knowledge and learn a new skill set at an architectural practice in London.</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="category-container write">
+              <div className="category"></div>
+            </div>
+            <div className="category-container art">
+              <div className="category"></div>
+            </div>
           </div>
-
         </div>
       </div>
     </>
@@ -100,3 +82,35 @@ function AboutMe() {
 }
 
 export default AboutMe
+
+{/* <div className="category">
+                
+              </div> */}
+{/* <div className="category">
+                <div className="category-wrapper">
+                  <div className="info">
+                    <div
+                      className="info-logo">
+                      <img src={arch} />
+                    </div>
+                  </div>
+                </div>
+                <div className="info-wrapper gd">
+                  <div className="gd-item">
+                    <div className="gd-logo" >
+                      <img src={udemy} />
+                    </div>
+                    <div className="gd-content">
+                      <p>I am currently studying China Studies as part of my year abroad.</p>
+                    </div>
+                  </div>
+                  <div className="gd-item">
+                    <div className="gd-logo">
+                      <img src={fiver} />
+                    </div>
+                    <div className="gd-content">
+                      <p>My goal is to practice my knowledge and learn a new skill set at an architectural practice in London.</p>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
