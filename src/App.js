0f668691585f937576a1c289825fd1630ab9014d
tobil <tobilesi@gmail.com>
2021-09-29 18:41:19 +0100

    final page bug fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import LandingPage from './components/LandingPage'
+import Main from './components/Main'
 import PageScroll from './components/landingComponents/PageScroll'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/scroll" component={LandingPage} />
+        <Route exact path="/main" component={Main} />
         <Route exact path="/" component={PageScroll} />
       </Switch>
     </BrowserRouter>
