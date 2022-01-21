02303a554fb91350a40ac35f2ced2e3bd7c03767
tobil <tobilesi@gmail.com>
2022-01-21 17:36:37 +0000

    title font added

--- src/components/architecture/wellnessCentre/sections/title.js
+++ src/components/architecture/wellnessCentre/sections/title.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import mainImg from '../../../../images/architectureImages/wcImages/mainEntrance.png'
+import mainImg from '../../../../images/architectureImages/wcImages/mainEntrance.png'
 
 
 function Title() {
@@ -7,14 +7,13 @@ function Title() {
   return (
     <>
       <div className='s-one'>
-        <div className='title-container'
-          // style={{
-          //   background: `url(${mainImg})`,
-          // }}
-        >
-          <h1 className='title-content'>WellnessCentre</h1>
+        <div className='title-container'>
+          <h1 className='title-content'>
+            Wellness Centre
+          </h1>
+          <img src={mainImg} />
         </div>
-        <footer>
+        <footer className='pageName'>
           <h6>Wellness Centre</h6>
         </footer>
       </div>
