67023907f94d5a6c4b2b9620f35ad68e99ef1814
Tobi <tobilesi@gmail.com>
2023-08-30 23:34:21 +0100

    chapter1 skeleton done with videos

--- src/components/tanzania/Chapter2.js
+++ src/components/tanzania/Chapter2.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import UmVideo from './UmVideo'
+// import UmVideo from './UmVideo'
 import Diagram from './Diagram'
 
 // videos
-import meetingMaasai from '../../images/tanzaniaVideos/meetingMaasai.mov'
+// import meetingMaasai from '../../images/tanzaniaVideos/meetingMaasai.mov'
 import waterBuilders from '../../images/tanzaniaImages/waterBuilders.png'
 import classRoom from '../../images/tanzaniaImages/classRoom.png'
 
@@ -14,9 +14,9 @@ import classRoom from '../../images/tanzaniaImages/classRoom.png'
 function Chapter2() {
   return (
     <>
-      <section className="sections">
+      {/* <section className="sections">
         <UmVideo content={meetingMaasai} />
-      </section>
+      </section> */}
       <section className="sections">
         <Diagram content={classRoom}  />
       </section>
